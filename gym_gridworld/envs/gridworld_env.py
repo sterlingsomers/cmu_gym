f3a0b8a49ced9a4fdd4262e3817e57cbc622c00a
sterlingsomers <psomers@andrew.cmu.edu>
2018-07-17 10:43:43 -0400

    Turned on rendering for fun

--- gym_gridworld/envs/gridworld_env.py
+++ gym_gridworld/envs/gridworld_env.py
@@ -295,7 +295,7 @@ class GridworldEnv(gym.Env):
 
         #return (self.map_volume, 0, True, crash)
 
-
+        self._render()
 
         return 0
 
@@ -398,11 +398,13 @@ class GridworldEnv(gym.Env):
         return observation
   
     def _render(self, mode='human', close=False):
-        return
-        if self.verbose == False:
-            return
-        img = self.observation
-        fig = plt.figure(self.this_fig_num)
+        #return
+        #if self.verbose == False:
+        #    return
+        #img = self.observation
+        img = self.map_volume['flat']
+        #fig = plt.figure(self.this_fig_num)
+        fig = plt.figure(0)
         plt.clf()
         plt.imshow(img)
         fig.canvas.draw()
