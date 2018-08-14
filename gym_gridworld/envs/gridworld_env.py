8bfe25e0f165b13173e8ae8ccbdb132a9eb83c9e
sterlingsomers <psomers@andrew.cmu.edu>
2018-08-14 16:48:37 -0400

    Added APL interface stuff for demo

--- gym_gridworld/envs/gridworld_env.py
+++ gym_gridworld/envs/gridworld_env.py
@@ -42,7 +42,7 @@ class GridworldEnv(gym.Env):
         self.verbose = True # to show the environment or not
         self.restart_once_done = True  # restart or not once done
         self.drop = False
-        self.maps = [(400,35), (350,90), (430,110),(390,50), (230,70)] #[(86, 266)] (70,50) # For testing, 70,50 there is no where to drop in the whole map
+        self.maps = [(230,70)]#[(400,35), (350,90), (430,110),(390,50), (230,70)] #[(86, 266)] (70,50) # For testing, 70,50 there is no where to drop in the whole map
         self.dist_old = 1000
         self.drop_package_grid_size_by_alt = {1: 3, 2: 5, 3: 7}
         self.factor = 5
