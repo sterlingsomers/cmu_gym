6f3c85bbbc11d56162dd28dfafa870ac01a82b9e
sterlingsomers <psomers@andrew.cmu.edu>
2018-08-22 13:36:02 -0400

    local commit for sterling's setup

--- run_agent.py
+++ run_agent.py
@@ -408,7 +408,7 @@ def main():
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         running = False
-                #sleep(1.5)
+                sleep(1.5)
                 # Timestep counter
                 t=0
                 rewards = []
@@ -446,7 +446,7 @@ def main():
                     screen_mssg_variable("Reward: ", np.round(reward,3), (168, 372))
                     pygame.display.update()
                     pygame.event.get()
-                    #sleep(1.5)
+                    sleep(1.5)
 
                     # BLIT!!!
                     # First Background covering everyything from previous session
@@ -458,7 +458,7 @@ def main():
                     # Update finally the screen with all the images you blitted in the run_trained_batch
                     pygame.display.update() # Updates only the blitted parts of the screen, pygame.display.flip() updates the whole screen
                     pygame.event.get() # Show the last state and then reset
-                    #sleep(1.2)
+                    sleep(1.2)
                     t += 1
 
                     # Dropping Agent
@@ -506,7 +506,7 @@ def main():
                             screen_mssg_variable("Reward: ", np.round(reward, 3), (168, 372))
                             pygame.display.update()
                             pygame.event.get()
-                            #sleep(1.5)
+                            sleep(1.5)
 
                             if action == 15:
                                 # The update of the text will be at the same time with the update of state
@@ -514,7 +514,7 @@ def main():
                                 pygame.display.update()
                                 pygame.event.get()  # Update the screen
                                 dictionary[nav_runner.episode_counter]['pack_hiker_dist'] = drop_runner.envs.pack_dist
-                                #sleep(1.5)
+                                sleep(1.5)
 
                             gameDisplay.fill(DARK_BLUE)
                             map_xy = obs[0]['img']
@@ -524,7 +524,7 @@ def main():
                             # Update finally the screen with all the images you blitted in the run_trained_batch
                             pygame.display.update()  # Updates only the blitted parts of the screen, pygame.display.flip() updates the whole screen
                             pygame.event.get()  # Show the last state and then reset
-                            #sleep(1.2)
+                            sleep(1.2)
                             t = t +1
 
                         dictionary[nav_runner.episode_counter]['observations'] = mb_obs
