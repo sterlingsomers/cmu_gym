0a37824dcc3d1be375f6266f2837d109c0431ff1
sterlingsomers <psomers@andrew.cmu.edu>
2018-08-27 13:17:30 -0400

    fix example

--- drop_prototype.py
+++ drop_prototype.py
@@ -179,9 +179,9 @@ chks.append(['isa','decision','one',['one',2],'two',['two',2],'three',['three',2
 #211
 #211
 
-chks.append(['isa', 'decision','one',['one',2],'two',['two',3],'three',['three',3],
-             'four',['four',2],'five',['five',3],'six',['six',3],
-             'seven',['seven',2],'eight',['eight',3],'nine',['nine',3],
+chks.append(['isa', 'decision','one',['one',2],'two',['two',1],'three',['three',1],
+             'four',['four',2],'five',['five',1],'six',['six',1],
+             'seven',['seven',2],'eight',['eight',1],'nine',['nine',1],
              'delta_x',['delta_x',-1],'delta_y',['delta_y',0]])
 
 
@@ -192,9 +192,9 @@ for x in chks:
 
 
 #Probe
-chk = ['isa', 'observation', 'one', ['one',2], 'two', ['two',1],'three', ['three',1],
-             'four', ['four',2], 'five', ['five',1], 'six', ['six',1],
-             'seven', ['seven',2], 'eight', ['eight',1], 'nine', ['nine',1],'actual_x',['actual_x',0],'actual_y',['actual_y',1]]
+chk = ['isa', 'observation', 'one', ['one',1], 'two', ['two',1],'three', ['three',1],
+             'four', ['four',1], 'five', ['five',1], 'six', ['six',1],
+             'seven', ['seven',2], 'eight', ['eight',2], 'nine', ['nine',2],'actual_x',['actual_x',0],'actual_y',['actual_y',1]]
 chunk = actr.define_chunks(chk)
 actr.schedule_simple_event_now("set-buffer-chunk",
                                ['imaginal', chunk[0]])
