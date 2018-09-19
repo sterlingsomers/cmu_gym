5516b8a2c2784ad29358c85b87cfee036877788c
Constantinos <kmitsopou@gmail.com>
2018-09-19 15:39:23 -0400

    minor

--- gym_gridworld/envs/create_np_map.py
+++ gym_gridworld/envs/create_np_map.py
@@ -4,7 +4,7 @@ import numpy as np
 import itertools
 from gym_gridworld.envs.mapquery import terrain_request
 from pathlib import Path
-path='/Users/paulsomers/COGLE/gym-gridworld/gym_gridworld/envs/'
+path='./gym_gridworld/'
 # map = pickle.load(open('050070.dict','rb'))
 # top_left = (50,70)
 # #make the volume
@@ -68,22 +68,21 @@ def convert_map_to_volume_dict(x,y,map,width,height):
 
     for xy, feat in map.items():
         #print(feat[1], feature_value_map.keys())
-        id = '{} {}'.format(feat[1], feat[0])
-        if id not in list(feature_value_map.keys()):
+        if feat[1] not in list(feature_value_map.keys()):
             #feature_value_map[feat[1]] = {}
 
             #for i in range(5):
-            feature_value_map[id] = {'val': value, 'color':color_map[id]}
-            value_feature_map[value] = {'feature':id[:-2], 'alt':float(id), 'color':color_map[id]}
+            feature_value_map[feat[1]] = {'val': value, 'color':color_map[feat[1]]}
+            value_feature_map[value] = {'feature':feat[1], 'alt':float(feat[0]), 'color':color_map[feat[1]]}
             value += 1
 
             #value += 20
         #put it in the flat
-        flat[xy[1] - top_left[1], xy[0] - top_left[0]] = feature_value_map[id]['val']
-        img[xy[1]- top_left[1], xy[0] - top_left[0], :] = feature_value_map[id]['color']
+        flat[xy[1] - top_left[1], xy[0] - top_left[0]] = feature_value_map[feat[1]]['val']
+        img[xy[1]- top_left[1], xy[0] - top_left[0], :] = feature_value_map[feat[1]]['color']
         #project it downwards through the volume
         for z in range(feat[0],-1,-1):
-            vol[z,xy[1] - top_left[1],xy[0] - top_left[0]] = feature_value_map[id]['val']
+            vol[z,xy[1] - top_left[1],xy[0] - top_left[0]] = feature_value_map[feat[1]]['val']
 
 
 
