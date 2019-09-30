e274b7ef4000e5c2dc19bd4627c132471c1b9dbc
sterlingsomers <psomers@andrew.cmu.edu>
2019-09-30 12:08:58 -0400

    modified the flatland to include an obstacle for training

--- run_keyboard_agent.py
+++ run_keyboard_agent.py
@@ -40,8 +40,8 @@ flags.DEFINE_integer("episodes", 1, "Number of complete episodes")
 
 
 #human subject flags
-flags.DEFINE_string("participant", 'Test', "The participants name")
-flags.DEFINE_string("map", 'flatland', "river, canyon, v-river, treeline, small-canyon, flatland")
+flags.DEFINE_string("participant", 'mike', "The participants name")
+flags.DEFINE_string("map", 'canyon', "flatland, canyon, v-river, treeline, small-canyon, flatland")
 flags.DEFINE_integer("configuration", 0, "0,1, or 2")
 
 
